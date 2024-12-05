cc250cb66cda7fcd3704baa1041b463d8e56add8
springboot20 <abbasopeyemi148@gmail.com>
2024-12-05 12:57:44 +0100

    added a log statement

--- myApp.js
+++ myApp.js
@@ -1,39 +1,6 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+console.log("hello world");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
