27047e5eec4cb106016b2f59367c8229550153e3
springboot20 <abbasopeyemi148@gmail.com>
2024-12-05 14:55:35 +0100

    modified server.js file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const myApp = require("./myApp");
 const express = require("express");
 const app = express();
 
-app.use("/public", express.static(__dirname + "public"));
+app.use("/public", express.static(__dirname + "/public"));
 
 if (!process.env.DISABLE_XORIGIN) {
   app.use((req, res, next) => {
